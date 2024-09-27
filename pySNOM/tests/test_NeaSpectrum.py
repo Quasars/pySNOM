cb78e15dc469a3460830e2527887e129cfd5b8ee
Gergely Nemeth <ngergihun@gmail.com>
2024-09-27 10:15:33 +0200

    corrected neaspectrum reader test

--- pySNOM/tests/test_NeaSpectrum.py
+++ pySNOM/tests/test_NeaSpectrum.py
@@ -4,15 +4,15 @@ import os
 import numpy as np
 
 import pySNOM
-from pySNOM import NeaSpectrum
+from pySNOM.readers import NeaSpectrumReader
 
 
 class test_Neaspectrum(unittest.TestCase):
     def test_readfile(self):
-        neasp = NeaSpectrum()
-        neasp.readNeaSpectrum(os.path.join(pySNOM.__path__[0], 'datasets/sp.txt'))
+        r = NeaSpectrumReader(os.path.join(pySNOM.__path__[0], 'datasets/sp.txt'))
+        data, params = r.read()
 
-        np.testing.assert_almost_equal(neasp.data['O1A'][0], 129.49686)
+        np.testing.assert_almost_equal(data['O1A'][0], 129.49686)
 
         
 if __name__ == '__main__':
